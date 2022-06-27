0ca66549794980fe5ab27c59a57743cf5bedc153
Mrq9rawi <moh.abdullah02@gmail.com>
2022-06-27 14:26:02 -0400

    Updates

---
+++ precache-manifest.d5cd5c5c7eaa170e566645882b88c4e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb07675d4a3bc37b771c",
+    "url": "/templatemo_563_seo_dream/static/css/main.96869073.chunk.css"
+  },
+  {
+    "revision": "eb07675d4a3bc37b771c",
+    "url": "/templatemo_563_seo_dream/static/js/main.eb07675d.chunk.js"
+  },
+  {
+    "revision": "ad2b29cdfb06374227e2",
+    "url": "/templatemo_563_seo_dream/static/js/1.ad2b29cd.chunk.js"
+  },
+  {
+    "revision": "cc2ed8c993e0d5256d4e",
+    "url": "/templatemo_563_seo_dream/static/js/runtime~main.cc2ed8c9.js"
+  },
+  {
+    "revision": "30639c1f452937f32c067d13ca010a52",
+    "url": "/templatemo_563_seo_dream/index.html"
+  }
+];
